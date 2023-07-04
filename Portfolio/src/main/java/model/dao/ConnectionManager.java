d737b989b854781bbe581e64f2a547443e8c140b
Frand162 <86558229+Frand162@users.noreply.github.com>
2023-07-04 12:00:59 +0900

    Update ConnectionManager.java

--- Portfolio/src/main/java/model/dao/ConnectionManager.java
+++ Portfolio/src/main/java/model/dao/ConnectionManager.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 /**
- * @author Yusuke Tanabe
  * データベースに接続するためのクラス。
  */
 public class ConnectionManager {
